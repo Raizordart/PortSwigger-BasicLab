aa281d4d0cf4839143bf5a118128043216956212
Raizord <89621601+Raizordart@users.noreply.github.com>
2026-03-16 17:51:53 +0700

    Delete Authentication vulnerabilities directory

--- Authentication vulnerabilities/script.py
+++
@@ -1,8 +0,0 @@
-with open("password.txt", "r") as f:
-    data = f.read().split()
-
-with open("password_modified.txt", "w") as m:
-    for i,dat in enumerate(data):
-        if(i%2 == 0):
-            m.write("peter\n")
-        m.write(f"{dat}\n")
